5d681bc26c81e6806dddb64205f562667c7e3add
Hard840 <jamgjust@gmail.com>
2024-04-29 21:05:33 +0600

    Zaeb

--- src/components/Home/HomeBlocks/PLNG/PLNG.jsx
+++ src/components/Home/HomeBlocks/PLNG/PLNG.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import '../PLNG/PLNG.css'
 const PLNG = () => {
     return (
-        <div>
+        <div className='PLNG'>
             <Link to='/'>
-                <div>HTML&CSS</div>
+                <div className='Lng'>HTML&CSS</div>
             </Link>
-
+            <Link to='/'>
+                <div className='Lng'>JavaScript</div>
+            </Link>
+            <Link to='/'>
+                <div className='Lng'>Phython</div>
+            </Link>
+            
         </div>
     );
 }
